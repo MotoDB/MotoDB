b453d958029f653b25c7a9ff8f0b8be6a7c8294b
Tommaso Bonato <tommasobonato95@gmail.com>
2016-06-09 17:42:35 +0200

    Added new classes in the model for Eng, Riders and Mechanics

---
+++ src/com/motodb/model/Mechanic.java
@@ -0,0 +1,5 @@
+package com.motodb.model;
+
+public class Mechanic extends Member {
+
+}
---
+++ src/com/motodb/model/Member.java
@@ -0,0 +1,152 @@
+package com.motodb.model;
+
+import java.util.Date;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Member {
+
+    private final IntegerProperty personalCode;
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty id;
+    private final StringProperty photo;
+    private final StringProperty birthplace;
+    private final StringProperty state;
+    private final StringProperty role;
+    private Date dateOfBirth;
+
+    public Member() {
+        this(0, null, null, null, null, null, null, null, null);
+    }
+
+    public Member(int personalCode, String firstName, String lastName, String id, String photo, String birthplace, String state,
+            String role, Date dateOfBirth) {
+        this.personalCode = new SimpleIntegerProperty(personalCode);
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.id = new SimpleStringProperty(id);
+        this.photo = new SimpleStringProperty(photo);
+        this.birthplace = new SimpleStringProperty(birthplace);
+        this.state = new SimpleStringProperty(state);
+        this.role = new SimpleStringProperty(role);
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    // Personal Code
+    public IntegerProperty personalCodeProperty() {
+        return personalCode;
+    }
+
+    public Integer getPersonalCode() {
+        return personalCode.get();
+    }
+
+    public void setPersonalCode(int firstName) {
+        this.personalCode.set(firstName);
+    }
+
+    // First Name
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    // First Name
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    // Personal Code
+    public String getId() {
+        return id.get();
+    }
+
+    public void setId(String id) {
+        this.id.set(id);
+    }
+
+    public StringProperty idProperty() {
+        return id;
+    }
+
+    // Photo URL
+    public StringProperty photoProperty() {
+        return photo;
+    }
+
+    public String getPhoto() {
+        return photo.get();
+    }
+
+    public void setPhoto(String photo) {
+        this.photo.set(photo);
+    }
+
+    // Birthplace
+    public StringProperty birthplaceProperty() {
+        return birthplace;
+    }
+
+    public String getBirthplace() {
+        return birthplace.get();
+    }
+
+    public void setBirthplace(String birthplace) {
+        this.birthplace.set(birthplace);
+    }
+
+    // State
+    public StringProperty stateProperty() {
+        return state;
+    }
+
+    public String getState() {
+        return state.get();
+    }
+
+    public void setState(String state) {
+        this.state.set(state);
+    }
+
+    // Role
+    public StringProperty roleProperty() {
+        return role;
+    }
+
+    public String getRole() {
+        return role.get();
+    }
+
+    public void setRole(String role) {
+        this.role.set(role);
+    }
+
+    // Date
+    public void setDate(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getDate() {
+        return this.dateOfBirth;
+    }
+
+}

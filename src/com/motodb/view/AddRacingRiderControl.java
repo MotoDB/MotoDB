fab4aa8978215f57baae0cf15d65a01ef4a1cee3
cgiulia4 <giulia.cecchetti96@gmail.com>
2016-06-17 20:13:22 +0200

    Racing Rider View

---
+++ src/com/motodb/view/AddRacingRiderControl.java
@@ -0,0 +1,246 @@
+package com.motodb.view;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import com.motodb.controller.ChampionshipManager;
+import com.motodb.controller.ChampionshipManagerImpl;
+import com.motodb.controller.ClaxManager;
+import com.motodb.controller.ClaxManagerImpl;
+import com.motodb.controller.RacingRiderManager;
+import com.motodb.controller.RacingRiderManagerImpl;
+import com.motodb.controller.SessionManager;
+import com.motodb.controller.SessionManagerImpl;
+import com.motodb.controller.WeekendManager;
+import com.motodb.controller.WeekendManagerImpl;
+import com.motodb.model.Byke;
+import com.motodb.model.Clax;
+import com.motodb.model.Manufacturer;
+import com.motodb.model.RacingRider;
+import com.motodb.model.Rider;
+import com.motodb.model.Session;
+import com.motodb.model.Weekend;
+import com.motodb.view.alert.AlertTypes;
+import com.motodb.view.alert.AlertTypesImpl;
+
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class AddRacingRiderControl extends ScreenControl {
+	
+	// Alert panel to manage exceptions
+    private final AlertTypes alert = new AlertTypesImpl();
+
+    // Controller
+    private final SessionManager sessionManager = new SessionManagerImpl();
+    private final ClaxManager classManager = new ClaxManagerImpl();
+    private final RacingRiderManager racingRiderManager = new RacingRiderManagerImpl();
+    private final WeekendManager weekendManager = new WeekendManagerImpl();
+    private final ChampionshipManager championshipManager = new ChampionshipManagerImpl();
+
+	@FXML
+	private TableView<RacingRider> racingRidersTable;
+	@FXML
+	private TableColumn<RacingRider, String> yearColumn, weekendColumn, riderColumn, classColumn, sessionColumn, 
+	timeColumn, finishedColumn, positionColumn, pointsColumn, speedColumn, manufacturerColumn, modelColumn;
+	@FXML
+	private TextField timeField, speedField, humidityField, groundTemperatureField, airTemperatureField, codeField, 
+	conditionsField, searchField;
+	
+	@FXML
+	private ComboBox<Session> sessionCodeBox;
+	@FXML
+	private ComboBox<String> yearBox;
+	@FXML
+	private ComboBox<Rider> riderBox;
+	@FXML
+	private ComboBox<Weekend> weekendBox;
+	@FXML
+	private ComboBox<Clax> classBox;
+	@FXML
+	private ComboBox<Boolean> finishedBox;
+	@FXML
+	private ComboBox<Manufacturer> manufacturerBox;
+	@FXML
+	private ComboBox<Integer> positionBox;
+	@FXML
+	private ComboBox<Integer> pointsBox;
+	@FXML
+	private ComboBox<Byke> bikeModelBox;
+		
+	@FXML
+	private Button delete;
+	@FXML
+	private VBox vBoxFields;
+
+    /**
+     * Called after the fxml file has been loaded; this method initializes 
+     * the fxml control class. 
+     */
+    public void initialize() {
+    	
+    	classBox.setItems(classManager.getClasses());
+    	championshipManager.getChampionships().forEach(l->yearBox.getItems().add(Integer.toString(l.getYear())));
+    	weekendBox.setDisable(true);
+    	
+    	// Initialize the table
+    	yearColumn.setCellValueFactory(cellData -> cellData.getValue().championshipYearProperty().asString());
+    	weekendColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getWeekendStartingDate().toString()));
+    	riderColumn.setCellValueFactory(cellData -> cellData.getValue().personalCodeProperty().asString());
+    	classColumn.setCellValueFactory(cellData -> cellData.getValue().classNameProperty());
+    	modelColumn.setCellValueFactory(cellData -> cellData.getValue().bikeModelProperty());
+    	sessionColumn.setCellValueFactory(cellData -> cellData.getValue().sessionCodeProperty());
+    	timeColumn.setCellValueFactory(cellData -> cellData.getValue().fastestTimeProperty());
+    	finishedColumn.setCellValueFactory(cellData -> cellData.getValue().finishedProperty().asString());
+    	positionColumn.setCellValueFactory(cellData -> cellData.getValue().positionProperty().asString());
+    	pointsColumn.setCellValueFactory(cellData -> cellData.getValue().pointsProperty().asString());
+    	speedColumn.setCellValueFactory(cellData -> cellData.getValue().averageSpeedProperty().asString());
+    	manufacturerColumn.setCellValueFactory(cellData -> cellData.getValue().manufacturerNameProperty());
+    	          
+        // Add observable list data to the table
+    	racingRidersTable.setItems(racingRiderManager.getRacingRiders());
+        
+        // Make the table columns editable by double clicking
+        this.edit();
+        // Use a 'searchField' to search for books in the tableView
+        this.search();
+        // Listen for selection changes and enable delete button
+        this.update();
+    }
+    
+    /**
+     * Called when the user press the 'add' button; this method adds
+     * a new depot to the controller ObservableList of depots
+     */
+	@FXML
+    private void add() {
+        try {
+        	
+        	racingRiderManager.addRacingRider(Integer.parseInt(yearBox.getSelectionModel().getSelectedItem()), weekendBox.getValue().getStartDate(), classBox.getValue().getName(), sessionCodeBox.getValue().getCode(),
+                    timeField.getText(), positionBox.getValue(), Integer.parseInt(speedField.getText()), finishedBox.getValue(), riderBox.getValue().getPersonalCode(), manufacturerBox.getValue().getManufacturerName(), bikeModelBox.getValue().getModel(),
+                    pointsBox.getValue());
+        
+        	racingRidersTable.setItems(racingRiderManager.getRacingRiders()); // Update table view
+        	this.clear();
+        } catch (Exception e) {
+        	e.printStackTrace();
+            alert.showWarning(e);
+        }
+    }
+	
+	/**
+     * Called when the user edit a depot name directly from the tableColumn;
+     * This method edits the selected field in the observableList of depots and 
+     * makes fields editable directly from the table
+     */
+	private void edit() {
+
+	}
+	
+	/**
+     * Called on delete button press, opens a confirmation dialog asking if you 
+     * really want to delete the element; this method is called 
+     * to delete the selected element from the observableList
+     */
+    @FXML
+    private void delete() {
+
+    }
+    
+    /**
+     * Called when the user enter something in the search field;
+     * It search name of the depot
+     */
+    private void search(){/*
+    	// 1. Wrap the ObservableList in a FilteredList (initially display all data).
+        FilteredList<Clax> filteredData = new FilteredList<>(manager.getClasses(), p -> true);
+
+        // 2. Set the filter Predicate whenever the filter changes.
+        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(e -> {
+                // If filter text is empty, display all persons.
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                // Compare first name and last name of every person with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (e.getName().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches first name.
+                } else if (e.getRules().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches last name.
+                }
+                return false; // Does not match.
+            });
+        });
+
+        // 3. Wrap the FilteredList in a SortedList. 
+        SortedList<Clax> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        sortedData.comparatorProperty().bind(classesTable.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        classesTable.setItems(sortedData);*/
+    }
+    
+    /**
+	 * It listen for selection changes to disable/enable the delete button 
+	 * when the user selects something in the table
+	 */
+	private void update(){
+		/*
+		yearBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			if(newValue!=null){
+				if(!weekendManager.getWeekendsFromYear(Integer.parseInt(newValue)).isEmpty()){
+					weekendBox.setDisable(false);
+					weekendBox.setItems(weekendManager.getWeekendsFromYear(Integer.parseInt(newValue)));
+					this.updatePossibleDates();
+				}else{
+					weekendBox.setDisable(true);
+					startDate.setDisable(true);
+					finishDate.setDisable(true);
+				}
+			}
+		});*/
+
+	}
+	
+	private void updatePossibleDates(){
+		/*weekendBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+			if(newValue!=null){
+				startDate.setDisable(false);
+				finishDate.setDisable(false);
+			    startDate.setValue(newValue.getStartDate().toLocalDate());
+			    finishDate.setValue(newValue.getStartDate().toLocalDate());
+		        final Callback<DatePicker, DateCell> dayCellFactory = 
+		            new Callback<DatePicker, DateCell>() {
+		                @Override
+		                public DateCell call(final DatePicker datePicker) {
+		                    return new DateCell() {
+		                        @Override
+		                        public void updateItem(LocalDate item, boolean empty) {
+		                            super.updateItem(item, empty);
+		                           
+		                            if (item.isBefore(newValue.getStartDate().toLocalDate()) || item.isAfter(newValue.getFinishDate().toLocalDate())) {
+		                                    setDisable(true);
+		                                    setStyle("-fx-background-color: #ffc0cb;");
+		                            }   
+		                    }
+		                };
+		            }
+		        };
+		        startDate.setDayCellFactory(dayCellFactory);
+		        finishDate.setDayCellFactory(dayCellFactory);
+			}
+		});*/
+	}
+}

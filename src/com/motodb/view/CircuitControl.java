70db3c066083f9fa6542e3721ee86dafc2a2e0a0
cgiulia4 <giulia.cecchetti96@gmail.com>
2016-06-20 04:08:57 +0200

    Circuit

---
+++ src/com/motodb/view/CircuitControl.java
@@ -0,0 +1,66 @@
+package com.motodb.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.motodb.controller.CircuitManager;
+import com.motodb.controller.CircuitManagerImpl;
+import com.motodb.controller.SponsorManager;
+import com.motodb.controller.SponsorManagerImpl;
+import com.motodb.model.Circuit;
+import com.motodb.model.Sponsor;
+import com.motodb.view.util.CircuitGridPane;
+import com.motodb.view.util.SponsorGridPane;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+public class CircuitControl extends ScreenControl {
+
+    @FXML
+    private Button addTyre, addBrand, addSponsor;
+
+    private CircuitManager circuitManager = new CircuitManagerImpl();
+    private List<CircuitGridPane> list = new ArrayList<>();
+
+    @FXML
+    private HBox mainPane;
+
+    public CircuitControl() {
+        super();
+    }
+
+    /**
+     * Called after the fxml file has been loaded; this method initializes the
+     * fxml control class.
+     */
+    public void initialize() {
+
+        list.clear();
+
+        GridPane grid = new GridPane();
+
+        for (Circuit circuit : circuitManager.getCircuits()) {
+            CircuitGridPane sponsorPane = new CircuitGridPane(circuit);
+            list.add(sponsorPane);
+        }
+
+        int i = 0;
+        int riders = 0;
+
+        while (i < ((list.size()) / 4)) {
+            int j = 0;
+            while (j <= 3) {
+                grid.add(list.get(riders).getPane(), j, i);
+                j++;
+                riders++;
+            }
+            i++;
+        }
+        mainPane.getChildren().add(grid);
+
+    }
+
+}
